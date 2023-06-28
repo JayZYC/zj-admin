6d7822094e3d2f9962b4bb1e239d384873f4b453
张宇超 <584405019@qq.com>
2023-06-28 22:07:52 +0800

    init user, role tables

--- deploy/sql/init.sql
+++ deploy/sql/init.sql
@@ -1,19 +1,33 @@
 /*
 用户表创建
 */
-
-CREATE TABLE IF NOT EXISTS "user" (
-  "id" uuid PRIMARY KEY,
-  "username" varchar(40) COLLATE "utf8mb3_general_ci",
-  "phone" varchar(20) COLLATE "utf8mb3_general_ci" DEFAULT NULL,
-  "password" varchar(40) COLLATE "utf8mb3_general_ci" DEFAULT NULL,
-  "email" varchar(255) COLLATE "utf8mb3_general_ci" DEFAULT NULL,
-  "avatar" BYTEA COLLATE "utf8mb3_general_ci" DEFAULT NULL,
-  "create_time" date DEFAULT NULL,
-  "update_time" date DEFAULT NULL,
-  "delete_time" date DEFAULT NULL,
-  "nick_name" varchar(40) COLLATE "utf8mb3_general_ci" DEFAULT NULL,
-  "role_id" integer DEFAULT NULL,
-  "organization_id" uuid NOT NULL,
-  "status" integer DEFAULT NULL,
+DROP TABLE  IF NOT EXISTS "user";
+CREATE TABLE "user" (
+    "id" uuid PRIMARY KEY,
+    "username" varchar(40),
+    "phone" varchar(20) DEFAULT NULL,
+    "password" varchar(40)  DEFAULT NULL,
+    "email" varchar(255) DEFAULT NULL,
+    "avatar" BYTEA  DEFAULT NULL,
+    "create_time" date DEFAULT NULL,
+    "update_time" date DEFAULT NULL,
+    "delete_time" date DEFAULT NULL,
+    "nick_name" varchar(40)  DEFAULT NULL,
+    "role_id" integer DEFAULT NULL,
+    "status" integer DEFAULT NULL
 );
+
+INSERT INTO "user" VALUES (uuid_generate_v4(), 'root', '17314943215', '123456', NULL, NULL, now(), now(), NULL, '超级管理员', 1, 1);
+
+/*
+角色表创建
+*/
+
+CREATE TABLE IF NOT EXISTS "role" (
+    "id" uuid PRIMARY KEY,
+    "create_time" date DEFAULT NULL,
+    "update_time" date DEFAULT NULL,
+    "delete_time" date DEFAULT NULL,
+    "name" varchar(40)  DEFAULT NULL,
+    "access" integer[] DEFAULT NULL
+    );
--- model/role.go
+++ model/role.go
@@ -1,16 +1,14 @@
 package model
 
 import (
-	"github.com/google/uuid"
 	"github.com/lib/pq"
 )
 
 type (
 	Role struct {
 		Model
-		Name           string        `gorm:"not null;type:varchar(200)"`
-		OrganizationID uuid.UUID     `json:"OrganizationID,string"` // 组织ID
-		Access         pq.Int64Array `gorm:"type:integer[]"`        // 服务类型
+		Name   string        `gorm:"not null;type:varchar(200)"`
+		Access pq.Int64Array `gorm:"type:integer[]"` // 服务类型
 	}
 
 	Roles []Role
